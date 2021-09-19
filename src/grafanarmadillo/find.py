37e3b914169836fdc899deda7aa23d4e18c3956c
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-09-19 01:59:40 -0400

    can search for special "general" folder

--- src/grafanarmadillo/find.py
+++ src/grafanarmadillo/find.py
@@ -1,18 +1,23 @@
 """Find Grafana dashboards and folders"""
 
+from typing import Dict, List
+from grafana_api.grafana_face import GrafanaFace
+
 Dashboard = type
 Folder = type
 
-from typing import List
-from grafana_api.grafana_face import GrafanaFace
 
 class Finder(object):
 	def __init__(self, api: GrafanaFace) -> None:
 		super().__init__()
 		self.api = api
 
-	def get_dashboards(self, name) -> List[Dashboard]:
-		return self.api.search.search_dashboards(query=name, type_="dash-db")
+	def get_dashboards(self, name, folder_ids=None) -> List[Dashboard]:
+		return self.api.search.search_dashboards(query=name, type_="dash-db", folder_ids=folder_ids)
 
 	def get_folders(self, name) -> List[Folder]:
-		return self.api.search.search_dashboards(query=name, type_="dash-folder")
+		if name == "General":
+			return [self.api.folder.get_folder_by_id(0)]
+		else:
+			search_result = self.api.search.search_dashboards(query=name, type_="dash-folder")
+			return list(map(lambda sr: self.api.folder.get_folder(sr["uid"]), search_result))
--- tests/test_search.py
+++ tests/test_search.py
@@ -8,10 +8,15 @@ from grafanarmadillo.find import Finder
 def test_with_testcontainer(ro_demo_grafana):
 	requests.get(ro_demo_grafana[0].url)
 
-def test_find_dashboards_smoke(ro_demo_grafana):
+def test_find_dashboards__smoke(ro_demo_grafana):
 	f = Finder(ro_demo_grafana[1])
 	assert len(f.get_dashboards("0")) == 1
 	
-def test_find_folders_smoke(ro_demo_grafana):
+def test_find_folders__smoke(ro_demo_grafana):
 	f = Finder(ro_demo_grafana[1])
 	assert(len(f.get_folders("f0"))) == 1
+
+def test_find_folder__general_folder(ro_demo_grafana):
+	"""The 'General' folder is a special folder, so we synthesis it"""
+	f = Finder(ro_demo_grafana[1])
+	assert(len(f.get_folders("General"))) == 1
