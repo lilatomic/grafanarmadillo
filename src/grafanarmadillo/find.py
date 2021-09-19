4756e33e4a2404c30f3bd6ecd71815a027fcd63a
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-09-19 16:15:14 -0400

    refactor formatting folder_ids parameter

--- src/grafanarmadillo/find.py
+++ src/grafanarmadillo/find.py
@@ -1,6 +1,7 @@
 """Find Grafana dashboards and folders"""
 
 from typing import Dict, List
+from grafana_api.api import folder
 from grafana_api.grafana_face import GrafanaFace
 
 from grafanarmadillo.util import flat_map
@@ -20,14 +21,13 @@ class Finder(object):
 			self.api.search.search_dashboards(query=name, type_="dash-db")
 		))
 
-	def _enumerate_dashboards_in_folders(self, folder_ids):
-		return self.api.search.search_dashboards(query=None, type_="dash-db", folder_ids=folder_ids)
+	def _enumerate_dashboards_in_folders(self, folder_ids: List[str]):
+		folder_param = ','.join(folder_ids)
+		return self.api.search.search_dashboards(query=None, type_="dash-db", folder_ids=folder_param)
 
 	def get_dashboards_in_folders(self, folders: List[str]) -> List[Dashboard]:
 		folder_objects = flat_map(lambda folder_name: self.get_folders(name=folder_name), folders)
-		folder_ids = ','.join(map(lambda f: str(f['id']), folder_objects))
-		print(folder_ids)
-		return self._enumerate_dashboards_in_folders(folder_ids)
+		return self._enumerate_dashboards_in_folders(list(map(lambda f: str(f['id']), folder_objects)))
 
 	def get_folders(self, name) -> List[Folder]:
 		if name == "General":
