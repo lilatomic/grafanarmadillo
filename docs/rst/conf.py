ec12b985b8b0407504094b3b675bf35fba671c9b
Daniel Goldman <daniel.goldman2@mail.mcgill.ca>
2021-09-20 01:32:21 -0400

    [RELEASE] 0.0.4 from 0.0.3

--- docs/rst/conf.py
+++ docs/rst/conf.py
@@ -48,7 +48,7 @@ project = 'grafanarmadillo'
 year = '2021'
 author = 'lilatomic'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.0.3'
+version = release = '0.0.4'
 
 pygments_style = 'trac'
 templates_path = ['.']
