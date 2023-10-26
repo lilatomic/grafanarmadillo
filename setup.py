e5a036a969b9550007a457208a6cffcbd84d71bb
Daniel Goldman <danielgoldman4@gmail.com>
2023-10-26 17:07:22 -0400

    add click as optional dep

--- setup.py
+++ setup.py
@@ -17,18 +17,6 @@ def read(*names, **kwargs):
 		return fh.read()
 
 
-# previous approach used to ignored badges in PyPI long description
-# long_description = '{}\n{}'.format(
-#     re.compile(
-#         '^.. start-badges.*^.. end-badges',
-#         re.M | re.S,
-#         ).sub(
-#             '',
-#             read('README.rst'),
-#             ),
-#     re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read(join('docs', 'CHANGELOG.rst')))
-#     )
-
 long_description = "{}\n{}".format(
 	read("README.rst"), read(join("docs", "CHANGELOG.rst")),
 )
@@ -58,27 +46,13 @@ setup(
 	keywords=[],
 	python_requires=">=3.6, <4",
 	install_requires=["grafana-client~=3.0", "typing-extensions~=3.0;python_version<'3.8'"],
-	extras_require={
-		# eg:
-		#   'rst': ['docutils>=0.11'],
-		#   ':python_version=="2.6"': ['argparse'],
-	},
+	extras_require={'cli': ["click>8"]},
 	setup_requires=[
 		#   'pytest-runner',
 		#   'setuptools_scm>=3.3.1',
 	],
 	entry_points={
-		"console_scripts": []
+		"console_scripts": ["grafanarmadillo=grafanarmadillo.cmd:grafanarmadillo"]
 		#
 	},
-	# cmdclass={'build_ext': optional_build_ext},
-	# ext_modules=[
-	#    Extension(
-	#        splitext(relpath(path, 'src').replace(os.sep, '.'))[0],
-	#        sources=[path],
-	#        include_dirs=[dirname(path)]
-	#    )
-	#    for root, _, _ in os.walk('src')
-	#    for path in glob(join(root, '*.c'))
-	# ],
 )
