b4427fa2bbf58bc81101a6ad633cb447a1a6324c
Daniel Goldman <danielgoldman4@gmail.com>
2023-12-26 12:49:37 -0500

    add test for failure

--- tests/flow/flow_integration_test.py
+++ tests/flow/flow_integration_test.py
@@ -5,7 +5,14 @@ from pathlib import Path
 import pytest
 
 from grafanarmadillo.cmd import load_data, make_mapping_templator
-from grafanarmadillo.flow import Alert, Dashboard, FileStore, Flow, GrafanaStore
+from grafanarmadillo.flow import (
+	Alert,
+	Dashboard,
+	FileStore,
+	Flow,
+	FlowException,
+	GrafanaStore,
+)
 from tests.conftest import requires_alerting
 
 
@@ -23,7 +30,7 @@ def test_flow__dashboard(rw_shared_grafana, mapping, tmpdir):
 			store_tmpl=filestore,
 			flows=[
 				Dashboard(
-					name_obj="/flow/fi-d0",
+					name_obj="/flow0/fi-d0",
 					name_tmpl="/flow/fi-d0",
 					templator=templator,
 				)
@@ -44,7 +51,7 @@ def test_flow__alert(rw_shared_grafana, mapping, tmpdir):
 			store_tmpl=filestore,
 			flows=[
 				Alert(
-					name_obj="/flow/fi-a0",
+					name_obj="/flow1/fi-a0",
 					name_tmpl="/flow/fi-a0",
 					templator=templator,
 				)
@@ -65,12 +72,12 @@ def test_flow__mixed(rw_shared_grafana, mapping, tmpdir):
 			store_tmpl=filestore,
 			flows=[
 				Dashboard(
-					name_obj="/flow/fi-d0",
+					name_obj="/flow2/fi-d0",
 					name_tmpl="/flow/fi-d0",
 					templator=templator,
 				),
 				Alert(
-					name_obj="/flow/fi-a0",
+					name_obj="/flow2/fi-a0",
 					name_tmpl="/flow/fi-a0",
 					templator=templator,
 				)
@@ -80,6 +87,33 @@ def test_flow__mixed(rw_shared_grafana, mapping, tmpdir):
 
 	assert (tmpdir / "flow" / "fi-a0.json").exists()
 	assert (tmpdir / "flow" / "fi-d0.json").exists()
+
+
+def test_flow__failure(rw_shared_grafana, mapping, tmpdir):
+	tmpdir = Path(tmpdir)
+
+	def mk_flow(filestore, grafanastore, templator):
+		return Flow(
+			store_obj=grafanastore,
+			store_tmpl=filestore,
+			flows=[
+				Dashboard(
+					name_obj="/flow3/fi-d0",
+					name_tmpl="/flow/fail",
+					templator=templator,
+				),
+				Dashboard(
+					name_obj="/flow3/fi-d0",
+					name_tmpl="/flow/fi-d0",
+					templator=templator,
+				),
+			])
+
+	with pytest.raises(FlowException):
+		_do_flow_test(mapping, mk_flow, rw_shared_grafana, tmpdir)
+
+	assert (tmpdir / "flow" / "fi-d0.json").exists()
+	assert not (tmpdir / "flow" / "fail.json").exists()
 
 
 def _do_flow_test(mapping, mk_flow, rw_shared_grafana, tmpdir):
