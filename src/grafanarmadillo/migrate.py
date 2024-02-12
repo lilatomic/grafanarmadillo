ac0d7eeca8fa9f120493e43d6668da20e70d955c
Daniel Goldman <danielgoldman4@gmail.com>
2024-02-12 15:33:52 -0500

    try timing out waiting for containers

--- src/grafanarmadillo/migrate.py
+++ src/grafanarmadillo/migrate.py
@@ -1,5 +1,6 @@
 """Migrate from Classic to Unified alerting."""
 import contextlib
+import datetime
 import shutil
 from dataclasses import dataclass
 from pathlib import Path
@@ -29,22 +30,22 @@ class DockerContainer:
 def start_container(image_name, volume_path: Path, environment_vars: Dict[str, str]):
 	"""Start a container."""
 	client = docker.from_env()
-	volumes = {str(volume_path): {'bind': '/var/lib/grafana/grafana.db', 'mode': 'rw'}}
+	volumes = {str(volume_path): {"bind": "/var/lib/grafana/grafana.db", "mode": "rw"}}
 	container = client.containers.run(
 		image_name,
 		detach=True,
-		ports={'3000/tcp': 0},
+		ports={"3000/tcp": 0},
 		volumes=volumes,
 		environment=environment_vars,
 	)
 	container.reload()
-	host_port = container.attrs['NetworkSettings']['Ports']['3000/tcp'][0]['HostPort']
+	host_port = container.attrs["NetworkSettings"]["Ports"]["3000/tcp"][0]["HostPort"]
 	return DockerContainer(container, image_name, int(host_port))
 
 
 def read_container_logs(container: DockerContainer):
 	"""Read logs from the docker container."""
-	return container.container.logs().decode('utf-8')
+	return container.container.logs().decode("utf-8")
 
 
 def stop_container(container: DockerContainer):
@@ -62,9 +63,18 @@ def with_container(image_name, volume_path: Path, environment_vars: Dict[str, st
 		stop_container(container)
 
 
-def _wait_until_ready(container: DockerContainer):
+DEFAULT_TIMEOUT = datetime.timedelta(seconds=30)
+
+
+def _wait_until_ready(
+	container: DockerContainer,
+	timeout: datetime.timedelta = DEFAULT_TIMEOUT,
+):
 	"""Wait until container's readiness check passes."""
+	end = datetime.datetime.now() + timeout
 	while True:
+		if datetime.datetime.now() > end:
+			raise RuntimeError(f"Could not connect to container in {timeout}")
 		try:
 			if requests.get(f"http://localhost:{container.host_port}/api/health").ok:
 				break
@@ -72,7 +82,12 @@ def _wait_until_ready(container: DockerContainer):
 			pass
 
 
-def migrate(grafana_image, grafana_db: Path, output_directory: Path, extra_env_vars: Dict[str, str] = None) -> None:
+def migrate(
+	grafana_image,
+	grafana_db: Path,
+	output_directory: Path,
+	extra_env_vars: Dict[str, str] = None,
+) -> None:
 	"""Migrate from classic to Unified alerting."""
 	extra_env_vars = extra_env_vars or {}
 
@@ -85,6 +100,13 @@ def migrate(grafana_image, grafana_db: Path, output_directory: Path, extra_env_v
 
 		_wait_until_ready(container)
 
-		exporter = BulkExporter({'host': "localhost", 'port': container.host_port, 'auth': ("admin", "admin")}, output_directory)
+		exporter = BulkExporter(
+			{
+				"host": "localhost",
+				"port": container.host_port,
+				"auth": ("admin", "admin"),
+			},
+			output_directory,
+		)
 
 		exporter.run()
