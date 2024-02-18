26e3987c44daac4b052d0a2df148bc4073045aff
Daniel Goldman <danielgoldman4@gmail.com>
2024-02-18 00:35:10 -0500

    improve arg passing
    - pull mapping args into decorator
    - allow empty cfg (helps display help text)
    - normalise auth to tuple in all cases

--- src/grafanarmadillo/cmd.py
+++ src/grafanarmadillo/cmd.py
@@ -18,14 +18,17 @@ from grafanarmadillo.util import load_data
 load_file_help = """Should be encoded as json. You can pass this in as a string; or as file using 'file://path/to/file'"""
 auto_template_env_help = "The special value '$auto' will automatically provide a value by prepending a '$' to the keys of the grafana mapping"
 
-mapping_help = \
-	"Mapping of values to findreplace in the template. " + \
-	"The type should be a mapping of environment names (like 'template' or 'production') " + \
-	"to dicts of replacements (for example, `{\"region\": \"SouthWest\"}`). " + \
-	load_file_help
+mapping_help = (
+	"Mapping of values to findreplace in the template. "
+	+ "The type should be a mapping of environment names (like 'template' or 'production') "
+	+ 'to dicts of replacements (for example, `{"region": "SouthWest"}`). '
+	+ load_file_help
+)
 env_grafana_help = "Name of the environment in the mapping file for Grafana, found in the `--mapping` argument"
-env_template_help = "Name of the environment in the mapping file for the template, found in the `--mapping` argument. " + \
-	auto_template_env_help
+env_template_help = (
+	"Name of the environment in the mapping file for the template, found in the `--mapping` argument. "
+	+ auto_template_env_help
+)
 
 
 def make_grafana(config) -> GrafanaApi:
@@ -35,15 +38,27 @@ def make_grafana(config) -> GrafanaApi:
 	return GrafanaApi(**config)
 
 
+def with_template_options(f):
+	return click.option("--mapping", help=mapping_help)(
+		click.option("--env-grafana", help=env_grafana_help)(
+			click.option("--env-template", help=env_template_help)(f)
+		)
+	)
+
+
 @click.group()
 @click.option("--cfg", "-c", help=f"Config for connecting to Grafana. {load_file_help}")
 @click.pass_context
 def grafanarmadillo(ctx, cfg):
 	"""Template Grafana things."""
 	ctx.ensure_object(dict)
-	config = load_data(cfg)
+	if cfg:
+		config = load_data(cfg)
+		if isinstance(config.get("auth"), list):
+			config["auth"] = tuple(config["auth"])
+	else:
+		config = {}
 	ctx.obj["cfg"] = config
-	ctx.obj["gfn"] = make_grafana(config)
 
 
 @grafanarmadillo.group()
@@ -53,10 +68,8 @@ def dashboard():
 
 @dashboard.command(name="export")
 @click.option("--src", help="Path to the dashboard to capture")
-@click.option("--dst", help="Path to write the dashboard to", type=click.File('w'))
-@click.option("--mapping", help=mapping_help)
-@click.option("--env-grafana", help=env_grafana_help)
-@click.option("--env-template", help=env_template_help)
+@click.option("--dst", help="Path to write the dashboard to", type=click.File("w"))
+@with_template_options
 @click.pass_context
 def _export_dashboard(ctx, src, dst, mapping, env_grafana, env_template):
 	"""Capture a dashboard from Grafana."""
@@ -79,11 +92,9 @@ def export_dashboard(gfn: GrafanaApi, src: str, dst: IO, templator: Templator):
 
 
 @dashboard.command(name="import")
-@click.option("--src", help="Path of the template", type=click.File('r'))
+@click.option("--src", help="Path of the template", type=click.File("r"))
 @click.option("--dst", help="Path to write the dashboard to")
-@click.option("--mapping", help=mapping_help)
-@click.option("--env-grafana", help=env_grafana_help)
-@click.option("--env-template", help=env_template_help)
+@with_template_options
 @click.pass_context
 def _import_dashboard(ctx, src, dst, mapping, env_grafana, env_template):
 	"""Deploy a template to Grafana."""
@@ -112,10 +123,8 @@ def alert():
 
 @alert.command(name="export")
 @click.option("--src", help="Path to the alert to capture")
-@click.option("--dst", help="Path to write the alert to", type=click.File('w'))
-@click.option("--mapping", help=mapping_help)
-@click.option("--env-grafana", help=env_grafana_help)
-@click.option("--env-template", help=env_template_help)
+@click.option("--dst", help="Path to write the alert to", type=click.File("w"))
+@with_template_options
 @click.pass_context
 def _export_alert(ctx, src, dst, mapping, env_grafana, env_template):
 	"""Capture an alert from Grafana."""
@@ -138,11 +147,9 @@ def export_alert(gfn: GrafanaApi, src: str, dst: IO, templator: Templator):
 
 
 @alert.command(name="import")
-@click.option("--src", help="Path of the template", type=click.File('r'))
+@click.option("--src", help="Path of the template", type=click.File("r"))
 @click.option("--dst", help="Path to write the dashboard to")
-@click.option("--mapping", help=mapping_help)
-@click.option("--env-grafana", help=env_grafana_help)
-@click.option("--env-template", help=env_template_help)
+@with_template_options
 @click.pass_context
 def _import_alert(ctx, src, dst, mapping, env_grafana, env_template):
 	"""Deploy an alert from a template."""
